cf9d8318cfc2e1484043971cd01fa8aa7b06e106
Soham Desai <sohamdesai67@gmail.com>
2026-02-16 08:33:47 +0530

    Integrate all feature modules

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# main.py
+from add import add
+from sub import sub
+from mul import mul
 
 def main():
+    a = 10
+    b = 5
     print("Calculator App")
-    print("Addition:", add(10, 5))
-    print("Subtraction:", sub(10, 5))
-    print("Multiplication:", mul(10, 5))
+    print("Addition:", add(a, b))
+    print("Subtraction:", sub(a, b))
+    print("Multiplication:", mul(a, b))
 
 if __name__ == "__main__":
     main()
